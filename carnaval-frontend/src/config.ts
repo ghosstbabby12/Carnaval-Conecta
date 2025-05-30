a9b7c2f944b793fba44bd82786fb076883fe6850
Jesusdavid2004 <jesus.villotaa@campusucc.edu.co>
2025-05-30 16:15:45 -0500

    Delete carnaval-frontend directory

--- carnaval-frontend/src/config.ts
+++
@@ -1,2 +0,0 @@
-// src/config.ts
-export const API_BASE_URL = "http://localhost:9090/api/events";
--- carnaval-frontend/src/services/types/Event.ts
+++
@@ -1,9 +0,0 @@
-export interface EventData {
-  id?: number;
-  title: string;
-  location: string;
-  description: string;
-  date: string;
-  media?: File | null;
-  bannerUrl: string;
-}
